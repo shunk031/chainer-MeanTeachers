821997da08d48f46565878cfab17d7bbc39b0bea
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-07-11 23:28:23 +0900

    fix bug

--- mean_teachers/functions.py
+++ mean_teachers/functions.py
@@ -9,7 +9,7 @@ def sigmoid_rampup(current, rampup_length):
     if rampup_length == 0:
         return 1.0
     else:
-        current = np.clip(current, 0, 0, rampup_length)
+        current = np.clip(current, 0.0, rampup_length)
         phase = 1.0 - current / rampup_length
         return (np.exp(-0.5 * phase * phase))
 
